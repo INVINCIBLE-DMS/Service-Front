06bb507762fb0adb2df869fc43a51ce991818992
dutexion <dutexion@gmail.com>
2023-11-22 18:29:20 +0900

    chore :: 포지션 값

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,29 @@ import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Nav = () => {
-  return <Wrapper>
-    <img src="/imgs/Logo.svg" title="Aster" alt="" />
-    <Interactions>
-      <Button to="/"><img src="/imgs/icons/Home.svg" alt="" /></Button>
-      <Button to="/ranking"><img src="/imgs/icons/Trophy.svg" alt="" /></Button>
-      <Button to="/message"><img src="/imgs/icons/Message.svg" alt="" /></Button>
-      <Button to="/profile" style={{padding: 0}}><img src="/imgs/icons/DefaultProfile.svg" alt="" /></Button>
-    </Interactions>
-  </Wrapper>
-}
+  return (
+    <Wrapper>
+      <img src='/imgs/Logo.svg' title='Aster' alt='' />
+      <Interactions>
+        <Button to='/'>
+          <img src='/imgs/icons/Home.svg' alt='' />
+        </Button>
+        <Button to='/ranking'>
+          <img src='/imgs/icons/Trophy.svg' alt='' />
+        </Button>
+        <Button to='/message'>
+          <img src='/imgs/icons/Message.svg' alt='' />
+        </Button>
+        <Button to='/profile' style={{ padding: 0 }}>
+          <img src='/imgs/icons/DefaultProfile.svg' alt='' />
+        </Button>
+      </Interactions>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
+  position: fixed;
   width: 85px;
   height: 100vh;
   display: flex;
@@ -22,14 +33,15 @@ const Wrapper = styled.div`
   box-sizing: border-box;
   padding: 20px 0 20px 0;
   gap: 30px;
+  background-color: white;
   box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
-`
+`;
 
 const Interactions = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-`
+`;
 
 const Button = styled(Link)`
   display: flex;
@@ -37,9 +49,11 @@ const Button = styled(Link)`
   cursor: pointer;
   box-sizing: border-box;
   border-radius: 5px;
-  border: 1px solid rgba(0, 0, 0, 0.10);
+  border: 1px solid rgba(0, 0, 0, 0.1);
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.25);
   background: #ffffff;
   transition: 0.2s all;
-  &:hover { background: #f7f7f7; }
-`
+  &:hover {
+    background: #f7f7f7;
+  }
+`;
--- src/router/Layout.jsx
+++ src/router/Layout.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router";
-import { Nav } from "../components/Nav";
+import { Nav } from "../Components/Nav";
 import { styled } from "styled-components";
 
 export const Layout = () => {
-  return <Wrapper>
-    <Nav />
-    <Outlet />
-  </Wrapper>
-}
+  return (
+    <Wrapper>
+      <Nav />
+      <Outlet />
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-`
+`;

ac841086f5c5daf78c18343290a8656c92fbe706
dutexion <dutexion@gmail.com>
2023-11-22 15:06:33 +0900

    feat :: theme 생성 및 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { ThemeProvider } from "styled-components";
 import { Router } from "./router/Router";
 import { GlobalStyle } from "./styles/globalstyle";
+import { theme } from "./styles/theme";
 
 function App() {
-  return <>
-    <GlobalStyle />
-    <Router />
-  </>
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+const color = {
+  strongPink: "#FF49C2",
+  normalPink: "#FF7DD4",
+  lightPink: "#FFA0DF",
+  milkPink: "#FFC6EC",
+  whitePink: "#FFDEF4",
+  darkGray: "#939393",
+};
+
+export const theme = {
+  color,
+};

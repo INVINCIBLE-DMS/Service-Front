35ea22ba3749a178729430b16e7f10c0a9470896
six-standard <dbrrl1127@gmail.com>
2023-12-01 15:41:25 +0900

    feat :: api 세팅

---
+++ src/apis/Board/index.js
@@ -0,0 +1,41 @@
+import { instance } from "../axios";
+
+export const getPosts = async () => {
+  return await instance.get("/users/feed");
+}
+
+export const deletePost = async (id) => {
+  return await instance.delete(`/users/feed/${id}`);
+}
+
+export const postPost = async (data) => {
+  return await instance.post("/users/feed", data);
+}
+
+export const getPostDetail = async (id) => {
+  return await instance.get(`/comment/${id}`);
+}
+
+export const postComment = async (id, data) => {
+  return await instance.post(`/comment/${id}`, data);
+}
+
+export const patchComment = async (id, data) => {
+  return await instance.patch(`/comment/${id}`, data);
+}
+
+export const deleteComment = async (id) => {
+  return await instance.delete(`/comment${id}`);
+}
+
+export const getCoComent = async (id) => {
+  return await instance.get(`/co-comment/${id}`);
+}
+
+export const postCoComent = async (id, data) => {
+  return await instance.post(`/co-comment/${id}`, data);
+}
+
+export const deleteCoComent = async (id) => {
+  return await instance.delete(`/co-coments/${id}`);
+}
---
+++ src/apis/axios.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Cookies } from "react-cookie";
+
+const cookie = new Cookies();
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 3000
+})
+
+instance.interceptors.request.use(
+  res => {
+    const token = cookie.get("accessToken");
+    if(token) {
+      res.headers.Authorization = `Bearer ${token}`;
+    }
+    return res;
+  }, 
+  err => {
+    return err;
+  }
+)
+
+instance.interceptors.request.use(
+  res => {
+    return res;
+  },
+  err => {
+    return err;
+  }
+)

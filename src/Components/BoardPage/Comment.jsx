1c5aaff030a55aa4476942738f951deadeec6ef6
six-standard <dbrrl1127@gmail.com>
2023-12-05 22:56:02 +0900

    feat :: 댓글 컴포넌트 기능 추가

--- src/Components/BoardPage/Comment.jsx
+++ src/Components/BoardPage/Comment.jsx
@@ -1,15 +1,20 @@
 import { styled } from "styled-components"
 import { theme } from "../../styles/theme"
 import { SubComment } from "./SubComment"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Write } from "./Write"
-import { getCoComent, postCoComent } from "../../apis/Board"
+import { deleteComment, getCoComent, getPostDetail, patchComment, postCoComent } from "../../apis/Board"
 
-export const Comment = ({ profile, content, metaData }) => {
+export const Comment = ({ profile, content, metaData, setContent }) => {
   const [hidden, setHidden] = useState(true);
   const [subComments, setSubComments] = useState(undefined);
   const [comment, setComment] = useState("");
+  const [edit, setEdit] = useState({
+    content: content,
+    edit: false
+  });
   const { id } = metaData;
+  const text = useRef();
   
   useEffect(() => {
     if(!hidden) {
@@ -19,12 +24,47 @@ export const Comment = ({ profile, content, metaData }) => {
     }
   }, [hidden])
 
-  const handleComment = () => {
+  /* 기록을 해 두자면, 여기 코드는 문제가 많다.
+    댓글을 수정하거나 삭제하면 수정이나 삭제 등의 행동을 수행하고 나서 댓글들을 다시 불러와야 한다.
+    근데 댓글을 불러오는 기능 자체는 부모 컴포넌트인 자세히 보기 페이지에 있어, useState만 써서는 이런 방식으로 작성해야 했다. (props와 setState를 활용함)
+    이런 방식도 작동은 하지만, 매우 비효율적이고 알아보기도 어렵다.
+    나중에 React-Query를 활용해서 수정하는 게 좋을 것 같다.
+  */
+  const handleVisible = () => {
     setHidden(prev => !prev);
   }
 
-  const handleEditComment = (e) => {
+  const handleChange = (e) => {
     setComment(e.target.value);
+  }
+
+  const handleDeleteComment = () => {
+    deleteComment(id).then(() => {
+      getPostDetail(metaData.postId).then(res => {
+        setContent(prev => { return { ...prev, commentResponseList: res.data.commentResponseList} });
+      });
+    });
+  }
+
+  const handleChangeEdit = (e) => {
+    text.current.style.height = "auto";
+    text.current.style.height = text.current.scrollHeight + "px";
+    setEdit(prev => { return { ...prev, content: e.target.value}});
+  }
+
+  const handleEditSubmit = (e) => {
+    patchComment(id, { content: edit.content }).then(() => {
+      getPostDetail(metaData.postId).then(res => {
+        setContent(prev => { return { ...prev, commentResponseList: res.data.commentResponseList} });
+        setEdit({...edit, edit: false})
+      });
+    });
+  }
+
+  const handleEditComment = () => {
+    text.current.style.height = "auto";
+    text.current.style.height = content.split("\n").length*30 + "px";
+    setEdit(prev => { return { content: content, edit: !prev.edit}});
   }
 
   const handleSubmitComment = () => {
@@ -43,18 +83,25 @@ export const Comment = ({ profile, content, metaData }) => {
         <h1>{profile.name}</h1>
         <h2>{metaData.edited && "수정됨"}</h2>
       </Profile>
-      <Content>
+      <Content $edit={edit.edit}>
         <h1>{content}</h1>
-        <Bottom>
-          <div>
-            <img src="/imgs/icons/Like.svg" alt="" />
-            <h1>{metaData.likes}개</h1>
-          </div>
-          <div>
-            <img src="/imgs/icons/Comment.svg" alt="" title={hidden ? "대댓글 펼치기" : "대댓글 접기"} onClick={handleComment} />
-          </div>
-          <h2>수정</h2>
-          <h2>삭제</h2>
+        <textarea value={edit.content} onChange={handleChangeEdit} placeholder="수정할 댓글을 입력해주세요" ref={text} rows={1} />
+        <Bottom $edit={edit.edit}>
+          <Left $edit={edit.edit}>
+            <div>
+              <img src="/imgs/icons/Like.svg" alt="" />
+              <h1>{metaData.likes}</h1>
+            </div>
+            <div>
+              <img src="/imgs/icons/Comment.svg" alt="" title={hidden ? "대댓글 펼치기" : "대댓글 접기"} onClick={handleVisible} />
+            </div>
+            <h2 onClick={handleEditComment}>수정</h2>
+            <h2 onClick={handleDeleteComment}>삭제</h2>
+          </Left>
+          <Right $edit={edit.edit}>
+            <EditButton onClick={handleEditComment}>취소</EditButton>
+            <EditButton onClick={handleEditSubmit}>확인</EditButton>
+          </Right>
         </Bottom>
       </Content>
       <SubContent $hidden={hidden}>
@@ -63,7 +110,7 @@ export const Comment = ({ profile, content, metaData }) => {
             return <SubComment author={i.username} content={i.content} />
           })}
         </Comments>
-        <Write submitAction={handleSubmitComment} editAction={handleEditComment} value={comment} sub />
+        <Write submitAction={handleSubmitComment} editAction={handleChange} value={comment} sub />
       </SubContent>
     </Data>
   </Component>
@@ -109,17 +156,34 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   & > h1 {
+    display: ${({$edit}) => $edit && "none"};
     font-size: 20px;
     font-weight: 300;
     line-height: 30px;
     white-space: pre-wrap;
     word-wrap: break-word;
   }
+  & > textarea {
+    display: ${({$edit}) => !$edit && "none"};
+    font-size: 20px;
+    font-weight: 300;
+    line-height: 30px;
+    min-height: 30px;
+    border-bottom: 1px solid black;
+  }
 `
 
 const Bottom = styled.div`
   gap: 30px;
   display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: ${({$edit})=> $edit && "end"};
+`
+
+const Left = styled.div`
+  gap: 30px;
+  display: ${({$edit}) => $edit ? "none" : "flex"};
   align-items: center;
   width: 100%;
   padding: 0 10px 0 10px;
@@ -149,6 +213,24 @@ const Bottom = styled.div`
   }
 `
 
+const Right = styled.div`
+  gap: 5px;
+  display: ${({$edit}) => $edit ? "flex" : "none"};
+  margin-top: 5px;
+`
+
+const EditButton = styled.button`
+  background: ${theme.color.whitePink};
+  width: 70px;
+  padding: 5px;
+  border-radius: 15px;
+  cursor: pointer;
+  transition: 0.2s all;
+  &:hover {
+    background: ${theme.color.milkPink};
+  }
+`
+
 const SubContent = styled.div`
   display: ${({$hidden}) => $hidden ? "none" : "flex"};
   flex-direction: column;
@@ -164,4 +246,9 @@ const Comments = styled.div`
   border-radius: 10px 10px 0 0;
   box-sizing: border-box;
   background: ${theme.color.whitePink};
+`
+
+const Like = styled.img`
+  cursor: pointer;
+  filter: ${({$liked}) => $liked && "invert(80%)"};
 `
